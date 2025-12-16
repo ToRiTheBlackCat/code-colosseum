0bed2e37a1356ca098f59020bb33e5a5f906ea24
TriNHM <triminh0502@gmail.com>
2025-12-16 14:59:23 +0700

    [ADD] add base src BE

---
+++ src/CodeColosseum/CodeColosseum.Shared/Domain/Error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeColosseum.Shared.Domain
+{
+    public record Error(string Code, string Name)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+        public static readonly Error NullValue = new("Error.NullValue", "Null value was provided");
+    }
+}
