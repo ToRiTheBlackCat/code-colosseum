9e9039b483f66c34bf43c764a3c3ca99ba97b856
TriNHM <triminh0502@gmail.com>
2026-01-13 17:10:24 +0700

    [UPDATE] fix Error and Result base Entity

--- src/CodeColosseum/CodeColosseum.Shared/Domain/Error.cs
+++ src/CodeColosseum/CodeColosseum.Shared/Domain/Error.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CodeColosseum.Shared.Domain
 {
-    public record Error(string Code, string Name)
+    public record Error(string Code, string Description)
     {
         public static readonly Error None = new(string.Empty, string.Empty);
         public static readonly Error NullValue = new("Error.NullValue", "Null value was provided");
--- src/CodeColosseum/CodeColosseum.Shared/Domain/Result.cs
+++ src/CodeColosseum/CodeColosseum.Shared/Domain/Result.cs
@@ -1,18 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization; 
 
 namespace CodeColosseum.Shared.Domain
 {
     public class Result
     {
+        public string Message { get; }
+        public int ErrorCount { get; }
+        public string ErrorMessage { get; }
+
+        [JsonIgnore]
         public bool IsSuccess { get; }
+
+        [JsonIgnore]
         public bool IsFailure => !IsSuccess;
+
+        [JsonIgnore]
         public Error Error { get; }
 
-        protected Result(bool isSuccess, Error error)
+        protected Result(bool isSuccess, Error error, string message)
         {
             if (isSuccess && error != Error.None)
                 throw new InvalidOperationException();
@@ -22,24 +28,39 @@ namespace CodeColosseum.Shared.Domain
 
             IsSuccess = isSuccess;
             Error = error;
+
+            Message = message;
+            ErrorCount = isSuccess ? 0 : 1;
+            ErrorMessage = isSuccess ? string.Empty : error.Description;
         }
 
-        public static Result Success() => new(true, Error.None);
-        public static Result Failure(Error error) => new(false, error);
 
-        public static Result<T> Success<T>(T value) => new(value, true, Error.None);
-        public static Result<T> Failure<T>(Error error) => new(default, false, error);
+        public static Result Success(string message = "Operation successful")
+            => new(true, Error.None, message);
+
+        public static Result Failure(Error error, string message = "Operation failed")
+            => new(false, error, message);
+
+        public static Result<T> Success<T>(T value, string message = "Operation successful")
+            => new Result<T>(value, true, Error.None, message);
+
+        public static Result<T> Failure<T>(Error error, string message = "Operation failed")
+            => new Result<T>(default, false, error, message);
     }
 
     public class Result<T> : Result
     {
-        private readonly T? _value;
+        private readonly T? _data;
 
-        public Result(T? value, bool isSuccess, Error error) : base(isSuccess, error)
+        public T? Data => IsSuccess ? _data : default;
+
+        public Result(T? value, bool isSuccess, Error error, string message)
+            : base(isSuccess, error, message)
         {
-            _value = value;
+            _data = value;
         }
 
-        public T Value => IsSuccess ? _value! : throw new InvalidOperationException("Failure result has no value");
+        [JsonIgnore]
+        public T Value => IsSuccess ? _data! : throw new InvalidOperationException("Failure result has no value");
     }
 }
