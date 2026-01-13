fbd5c9a6d805c6cd62b49755262dbeb754b162dd
TriNHM <triminh0502@gmail.com>
2026-01-13 23:34:40 +0700

    [ADD] Update Role Api

--- src/CodeColosseum/CodeColosseum.API/Controllers/RolesController.cs
+++ src/CodeColosseum/CodeColosseum.API/Controllers/RolesController.cs
@@ -1,11 +1,14 @@
-﻿using MediatR;
+﻿using Azure.Core;
+using CodeColosseum.Shared.Domain;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Modules.Users.Application.RoleFeatures.Commands.CreateRole;
+using Modules.Users.Application.RoleFeatures.Commands.UpdateRole;
 
 namespace CodeColosseum.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/roles")]
     [ApiController]
     public class RolesController : ControllerBase
     {
@@ -27,6 +30,27 @@ namespace CodeColosseum.API.Controllers
             }
 
             return Ok(result); 
+        }
+
+        [HttpPut("{roleId}")]
+        public async Task<IActionResult> UpdateRole(Guid roleId, [FromBody] UpdateRoleCommand command)
+        {
+            // Đảm bảo ID trên URL khớp với ID trong Body (hoặc gán đè)
+            if (roleId != command.RoleId)
+            {
+                return BadRequest(Result.Failure<Guid>(
+                        new Error("","RoleId not matched"),"Update failed"
+                    ));
+            }
+
+            var result = await _sender.Send(command);
+
+            if (result.IsFailure)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
         }
     }
 }
---
+++ src/CodeColosseum/Modules.Users.Features/RoleFeatures/Commands/UpdateRole/UpdateRoleCommand.cs
@@ -0,0 +1,16 @@
+﻿using CodeColosseum.Shared.Application.Messaging;
+using Modules.Users.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.Users.Application.RoleFeatures.Commands.UpdateRole
+{
+    public record UpdateRoleCommand(
+        Guid RoleId,
+        string RoleName,
+        string Description
+    ) : ICommand<Guid>;
+}
