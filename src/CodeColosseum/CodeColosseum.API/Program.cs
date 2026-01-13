e68a8c43a5b159bb8a12e11c42892adad06aa215
TriNHM <triminh0502@gmail.com>
2026-01-13 12:15:26 +0700

    [ADD] configure Authen for swagger + CORS policy

--- src/CodeColosseum/CodeColosseum.API/Program.cs
+++ src/CodeColosseum/CodeColosseum.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Modules.Users.Application;
 using Modules.Users.Infrastructure;
 using System.Text.Json.Serialization;
@@ -5,6 +7,64 @@ using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+#region Configure JsonIgnoreCycles
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
+});
+#endregion
+
+builder.Services.AddEndpointsApiExplorer();
+
+#region Configure Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "COLLAB-SPHERE_API",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Example: \"Bearer {token}\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+#endregion
+
+#region Configure CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowCodeColosseumOrigins", policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:3000"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+
+    });
+});
+
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -38,7 +98,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowCodeColosseumOrigins");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
