058095f02c930c76042156a51c090c48372a0295
TriNHM <triminh0502@gmail.com>
2026-01-15 23:24:35 +0700

    [ADD] ValidationPipeline

---
+++ src/CodeColosseum/CodeColosseum.Shared/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using CodeColosseum.Shared.Application.Abstractions;
+using CodeColosseum.Shared.Utilities.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeColosseum.Shared
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddSharedInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Configure Email Settings from appsettings.json
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+            // Register Email Service
+            services.AddTransient<IEmailService, SmtpEmailService>();
+
+            return services;
+        }
+    }
+}
