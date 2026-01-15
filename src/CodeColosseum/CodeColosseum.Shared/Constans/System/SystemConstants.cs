b3e2f7538b6152a1d3193752f17d24898f606b64
TriNHM <triminh0502@gmail.com>
2026-01-15 23:22:50 +0700

    [ADD] role constants

---
+++ src/CodeColosseum/CodeColosseum.Shared/Constans/System/SystemConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeColosseum.Shared.Constans.System
+{
+    public static class SystemConstants
+    {
+        public static string EMAIL_CONFIRMATION_OTP = "EmailConfirmationOTP";
+    }
+}
---
+++ src/CodeColosseum/CodeColosseum.Shared/Constans/System/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeColosseum.Shared.Constans.System
+{
+    public static class UserRole
+    {
+        public const string ADMIN = "Admin";
+        public const string USER = "User";
+    }
+}
